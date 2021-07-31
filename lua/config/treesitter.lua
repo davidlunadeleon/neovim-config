6a28e1a04d722eb77ca7b3361cf094d3f538fbf4
David Luna de León <davidlunadeleon@gmail.com>
2021-08-01 00:18:43 +0200

    Create config files for specific plugins

---
+++ lua/config/treesitter.lua
@@ -0,0 +1,31 @@
+-- Treesitter configuration
+-- Parsers must be installed manually via :TSInstall
+require('nvim-treesitter.configs').setup {
+	ensure_installed = {
+		'bash',
+		'c',
+		'cmake',
+		'comment',
+		'cpp',
+		'css',
+		'dockerfile',
+		'gdscript',
+		'go',
+		'graphql',
+		'haskell',
+		'html',
+		'javascript',
+		'json',
+		'latex',
+		'python',
+		'regex',
+		'ruby',
+		'rust',
+		'svelte',
+		'typescript',
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+	}
+}
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,13 +2,15 @@ local execute = vim.api.nvim_command
 local fn = vim.fn
 
 -- Bootstrap packer
-
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
 	fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
 	execute 'packadd packer.nvim'
 end
+
+-- Run PackerCompile when modifying plugins.lua
+vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
 
 local use = require('packer').use
 require('packer').startup({function()
@@ -20,10 +22,17 @@ require('packer').startup({function()
 			'nvim-treesitter/nvim-treesitter-refactor',
 			'nvim-treesitter/nvim-treesitter-textobjects'
 		},
-		run = ':TSUpdate'
+		run = ':TSUpdate',
+		config = [[require('config.treesitter')]]
 	}
-	use 'neovim/nvim-lspconfig' -- Configurations for the built-in LSP client
-	use 'hrsh7th/nvim-compe' -- Autocompletion
+	use {
+		'neovim/nvim-lspconfig', -- Configurations for the built-in LSP client
+		config = [[require('config.lspconfig')]]
+	}
+	use {
+		'hrsh7th/nvim-compe', -- Autocompletion
+		config = [[require('config.compe')]]
+	}
 	use 'L3MON4D3/LuaSnip' -- Snippets
 end,
 config = {
@@ -31,3 +40,4 @@ config = {
 		open_fn = require('packer.util').float
     }
 }})
+
