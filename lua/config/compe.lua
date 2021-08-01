759c468a2180c1cf257f70cc231b9c92d50e0dd6
David Luna de León <davidlunadeleon@gmail.com>
2021-08-02 00:17:11 +0200

    Fix indentation in compe config

--- lua/config/compe.lua
+++ lua/config/compe.lua
@@ -14,16 +14,16 @@ require('compe').setup {
 
 -- Utility functions for compe and luasnip
 local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
+	return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 local check_back_space = function()
-  local col = vim.fn.col '.' - 1
-  if col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' then
-    return true
-  else
-    return false
-  end
+	local col = vim.fn.col '.' - 1
+	if col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' then
+		return true
+	else
+		return false
+	end
 end
 
 -- Use (s-)tab to:
@@ -32,25 +32,25 @@ end
 local luasnip = require 'luasnip'
 
 _G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t '<C-n>'
-  elseif luasnip.expand_or_jumpable() then
-    return t '<Plug>luasnip-expand-or-jump'
-  elseif check_back_space() then
-    return t '<Tab>'
-  else
-    return vim.fn['compe#complete']()
-  end
+	if vim.fn.pumvisible() == 1 then
+		return t '<C-n>'
+	elseif luasnip.expand_or_jumpable() then
+		return t '<Plug>luasnip-expand-or-jump'
+	elseif check_back_space() then
+		return t '<Tab>'
+	else
+		return vim.fn['compe#complete']()
+	end
 end
 
 _G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t '<C-p>'
-  elseif luasnip.jumpable(-1) then
-    return t '<Plug>luasnip-jump-prev'
-  else
-    return t '<S-Tab>'
-  end
+	if vim.fn.pumvisible() == 1 then
+		return t '<C-p>'
+	elseif luasnip.jumpable(-1) then
+		return t '<Plug>luasnip-jump-prev'
+	else
+		return t '<S-Tab>'
+	end
 end
 
 -- Map tab to the above tab complete functiones
