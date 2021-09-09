76ca87ba2c62eb1617ee7aac97c1856ec65f7eda
David Luna de León <davidlunadeleon@gmail.com>
2021-09-09 10:14:08 -0500

    Remove lines from lspinstall config

--- lua/config/lspinstall.lua
+++ lua/config/lspinstall.lua
@@ -4,20 +4,6 @@ local function setup_servers()
 	require'lspinstall'.setup()
 	local servers = require'lspinstall'.installed_servers()
 
-	table.insert(servers, "cpp")
-	table.insert(servers, "bash")
-	table.insert(servers, "css")
-	table.insert(servers, "dockerfile")
-	table.insert(servers, "go")
-	table.insert(servers, "haskell")
-	table.insert(servers, "html")
-	table.insert(servers, "json")
-	table.insert(servers, "graphql")
-	table.insert(servers, "python")
-	table.insert(servers, "rust")
-	table.insert(servers, "svelte")
-	table.insert(servers, "typescript")
-
 	for _, server in pairs(servers) do
 		require'lspconfig'[server].setup{}
 	end
