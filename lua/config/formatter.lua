6bd34c10ffa9362cc1cb486c47b058ca3fd0c530
David Luna de León <davidlunadeleon@gmail.com>
2022-03-07 08:44:49 -0600

    Update formatter config for prettier.

--- lua/config/formatter.lua
+++ lua/config/formatter.lua
@@ -1,13 +1,16 @@
+local prettier = function()
+	return {
+		exe = "prettier",
+		args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
+		stdin = true
+	}
+end
+
 require('formatter').setup({
 	filetype = {
-		javascript = {
-			function()
-				return {
-					exe = "prettier",
-					args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-					stdin = true
-				}
-			end
-		},
+		javascript = {prettier},
+		json = {prettier},
+		html = {prettier},
+		yaml = {prettier},
 	}
 })
