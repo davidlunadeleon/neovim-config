0822c8da8e6daf03555e6f3ba5f75a2fd19eee19
David Luna de León <davidlunadeleon@gmail.com>
2022-07-16 09:11:42 -0500

    Add formatter to new langs

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_exec([[
 augroup FormatAutoGroup
 	autocmd!
-	autocmd BufWritePost *.js,*.py,*.json,*.html,*.yaml,*.svelte FormatWrite
+	autocmd BufWritePost *.js,*.py,*.json,*.html,*.yaml,*.svelte,*ts,*cpp,*c,*css FormatWrite
 augroup END
 ]], true)
--- lua/config/formatter.lua
+++ lua/config/formatter.lua
@@ -1,11 +1,15 @@
 require('formatter').setup({
 	filetype = {
+		c = {require("formatter.filetypes.c").clangformat},
+		cpp = {require("formatter.filetypes.cpp").clangformat},
+		css = {require("formatter.filetypes.css").prettier},
+		html = {require("formatter.filetypes.html").prettier},
 		javascript = {require("formatter.filetypes.javascript").prettier},
 		json = {require("formatter.filetypes.json").prettier},
-		html = {require("formatter.filetypes.html").prettier},
-		yaml = {require("formatter.filetypes.yaml").prettier},
 		python = {require("formatter.filetypes.python").black},
 		svelte = {require("formatter.filetypes.svelte").prettier},
+		typescript = {require("formatter.filetypes.typescript").prettier},
+		yaml = {require("formatter.filetypes.yaml").prettier},
 		["*"] = {
 			require("formatter.filetypes.any").remove_trailing_whitespace
 		}
