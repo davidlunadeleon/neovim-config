1e59600fecad8ffe2432b5f2deead71d2dd6c1fa
David Luna de León <davidlunadeleon@gmail.com>
2024-08-11 11:47:27 -0700

    Setup treesitter config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,4 +3,13 @@ return {
 	build = function()
 		require('nvim-treesitter.install').update({ with_sync = true })()
 	end,
+	config = function()
+		local configs = require('nvim-treesitter.configs')
+		configs.setup({
+			ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'javascript', 'html' },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end
 }
