c8d6a2a5b1866578abdc1623e717b6cdd4704c3b
David Luna de León <davidlunadeleon@gmail.com>
2024-08-11 12:08:25 -0700

    Keys for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,12 @@ return {
 	'nvim-telescope/telescope.nvim', 
 	tag = '0.1.8',
 	dependencies = {
-		'nvim-lua/plenary.nvim'
-	}
+		'nvim-lua/plenary.nvim',
+	},
+	keys = {
+		{ '<leader>ft', '<cmd>:Telescope<cr>', desc = 'Telescope' },
+		{ '<leader>ff', '<cmd>:Telescope find_files<cr>', desc = 'Telescope find files' },
+		{ '<leader>fg', '<cmd>:Telescope live_grep<cr>', desc = 'Telescope live grep' },
+	},
 }
 
