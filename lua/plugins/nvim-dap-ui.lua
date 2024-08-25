b96a07e82b8172f221e8f4e4473ea1359de9d289
David Luna de León <davidlunadeleon@gmail.com>
2024-08-25 16:32:27 -0700

    Keys for nvim-dap-ui

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -1,1 +1,10 @@
-return { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } }
+return {
+	"rcarriga/nvim-dap-ui",
+	dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+	config = function()
+		require('dapui').setup {}
+	end,
+	keys = {
+		{ "<leader>dui", "<cmd>:lua require'dapui'.toggle()<cr>", desc = "Toggle DAP UI" },
+	}
+}
